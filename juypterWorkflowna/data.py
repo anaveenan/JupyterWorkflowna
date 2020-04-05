2257577b9f976ec46d2f473112927998e3c343e8
naveenan <anaveenan@gmail.com>
2020-04-05 18:17:06 -0500

    Bug:fix

--- juypterWorkflowna/data.py
+++ juypterWorkflowna/data.py
@@ -14,7 +14,7 @@ def get_fremont_data(filename='Fremont.csv',url=FREMONT_URL,forceDownload=False)
     forceDownload:bool(optional)
         if True,force redownload data
     Returns
-    _______
+    __________
     data: pandas.DataFrame
     Fremont bridge dataset
 
@@ -22,7 +22,15 @@ def get_fremont_data(filename='Fremont.csv',url=FREMONT_URL,forceDownload=False)
     if forceDownload or not os.path.exists(filename):
         urlretrieve(FREMONT_URL,'Fremont.csv')
 
-    data=pd.read_csv('Fremont.csv',index_col='Date',parse_dates=True).drop(['Fremont Bridge Total'],axis=1)
+    #data=pd.read_csv('Fremont.csv',index_col='Date',parse_dates=True)
+
+    data=pd.read_csv('Fremont.csv',index_col='Date').drop(['Fremont Bridge Total'],axis=1)
+    try:
+        data.index=pd.to_datetime(data.index,format='%m/%d/%Y %I:%S:%M %p')
+    except TypeError:
+        data.index=pd.to_datetime(data.index)
+
+
     data.columns=['East','West']
     data=data.assign(Total=lambda x:x['East']+x['West'])
     
